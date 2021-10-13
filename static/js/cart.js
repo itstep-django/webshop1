8571db115bfe10c7e4a64824f81868344e9b8c9a
Vladimir <45636169+softmasterlab@users.noreply.github.com>
2021-10-13 22:07:32 +0300

    Добавлены регистрация, авторизация и каталог товаров

---
+++ static/js/cart.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    $('.add-to-cart-btn').click(function() {
+        let productId = $(this).prev().val();
+
+        alert('Select product with ID: ' + productId);
+    });
+
+});
